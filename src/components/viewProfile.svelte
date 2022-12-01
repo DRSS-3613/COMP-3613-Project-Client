f0abb333f424db1800c20ccdc73175139594f862
Daniel Yatali <danielyatali902@gmail.com>
2022-12-01 17:34:51 -0400

    setup default profile view

---
+++ src/components/viewProfile.svelte
@@ -0,0 +1,19 @@
+<div class="md:flex items-center  bg-white border w-full h-auto">
+	<div class="w-full h-full flex justify-center">
+		<img class="object-fill" src="/user.png" alt="" />
+	</div>
+	<div class="">
+		<p class="text-gray-400">
+			Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+			chronological order.
+		</p>
+		<p class="text-gray-400">
+			Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+			chronological order.
+		</p>
+		<p class="text-gray-400">
+			Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+			chronological order.
+		</p>
+	</div>
+</div>
